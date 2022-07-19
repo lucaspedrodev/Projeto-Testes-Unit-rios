37f05ed2fe7702dad5a1387ff4b9f597c2e8c082
Lucas Pedro <rodrigerioo@gmail.com>
2022-07-19 18:52:02 -0300

    exercicio 03 completo

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,14 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
-
+const vqv = (string, numero) => {
+if (string === undefined && numero === undefined) {
+  return undefined;
+}
+let texto = `Oi, meu nome é ${string}!
+Tenho ${numero} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return texto;
+};
 module.exports = vqv;
